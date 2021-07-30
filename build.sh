5f68a6735ee7033c4048eb43ff2d13d541563b98
Mome Borogove <rdadolf+public@gmail.com>
2021-07-30 15:52:22 -0700

    Add lib40k as submodule.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 data-scripts/gen_data_js.py data/VERSION data/enchantments.cfg data/Lang_Artifacts.xml data/inventoryitems.cfg data.js
+python3 scripts/gen_data_js.py data/VERSION data/enchantments.cfg data/Lang_Artifacts.xml data/inventoryitems.cfg data.js
 sass --no-source-map scss:css
