b60291b8f0cbfb2b8f0a8043a6633e6b92cb7b0b
Mome Borogove <rdadolf+public@gmail.com>
2021-07-30 15:56:09 -0700

    Relocate submodule.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 scripts/gen_data_js.py data/VERSION data/enchantments.cfg data/Lang_Artifacts.xml data/inventoryitems.cfg data.js
+PYTHONPATH=lib40k/ python3 scripts/gen_data_js.py data/VERSION data/enchantments.cfg data/Lang_Artifacts.xml data/inventoryitems.cfg data.js
 sass --no-source-map scss:css
