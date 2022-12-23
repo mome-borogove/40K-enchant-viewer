6d056ab1925c3e570236169a90a896896d6aaeae
Mome Borogove <rdadolf+public@gmail.com>
2022-12-23 07:42:59 -0800

    Fixed item type filtering for belts and armor.
    Switched back to using the <Types> section for item type names.
    Manually patch-in fixes for the Battle Sister items.
    
    Thanks to Nyxie the Fae on Discord for pointing out the bug.

--- data-scripts/parse_langs.py
+++ data-scripts/parse_langs.py
@@ -2,6 +2,29 @@
 
 from xml.etree import ElementTree as ETree
 
+# Neocore decided to give all of the Battle Sister weapons the same type name
+# for some reason. Oh they have different names in the <Items> section, but
+# *that* section is missing a ton of items altogether. Without a rational way
+# to reconcile these two sections, I've elected to just manually override
+# the duplicate names. At least we won't be missing anything.
+# NOTE: I'm using raw &'s here, because these labels will be on buttons,
+# which don't interpret HTML.
+# FIXME: In the future, if TA construct items get added, they should get
+# added here, too. They're in the same boat.
+EXCEPTIONS={
+  'c_blessed_blade_rod': 'Blessed Blade & Rod',
+  'c_bolt_pistol_chainsword': 'Bolt Pistol & Chainsword',
+  'c_chainsword_rod': 'Chainsword & Rod',
+  'c_inferno_pistol_chainsword': 'Inferno Pistol & Chainsword',
+  'c_plasma_pistol_blessed_blade': 'Plasma Pistol & Blessed Blade',
+  'c_plasma_pistol_brazier': 'Plasma Pistol & Brazier',
+  'c_hand_flamer_brazier': 'Hand Flamer & Brazier',
+  'c_hand_flamer_rod': 'Hand Flamer & Rod',
+  'power_halberd': 'Power Halberd',
+  'power_maul': 'Power Maul',
+  # Why would you do this!? Literally every other gun isn't done this way!
+  'flamer': 'Flamer',
+}
 
 def parse_langs(file):
   root = ETree.parse(file)
@@ -16,27 +39,26 @@ def parse_langs(file):
     ench_str_map[str.lower(ench.tag)] = ench.find('desc').find('eng').text
 
   # Build the item type string map
-  item_types = artifacts.find('Items')
+  item_types = artifacts.find('Types')
   item_type_map = {}
+
   for item_type in item_types:
-    # Neocore is anything but consistent in their XML. Originally, we looked
-    # in the 'Types' section, but the Battle Sister update screwed that up.
-    # Instead, all the combo items are named identically, so we have to look in
-    # 'Items'. Except *that* has bugs where some items only have an 'eng' tag
-    # and not a 'Name' tag above it. So we have to do this hacky garbage.
-    name_element = item_type.find('Name')
-    if name_element is None:
-      # ...really?
-      name_element = item_type.find('name')
-    if name_element is None:
-      eng_element = item_type.find('eng')
-    else:
-      eng_element = name_element.find('eng')
-    if eng_element is None:
-      print('Warning: item type',str(item_type.tag),'has no name or text. If this is not an equippable item, it can be ignored.')
-    else:
-      name_string = eng_element.text
-    item_type_map[str.lower(item_type.tag)] = name_string
+    item_type_map[str.lower(item_type.tag)] = item_type.find('eng').text
+
+  # Manually hack-in some exceptions to fix Neocore's inconsistencies.
+  for item_type in EXCEPTIONS:
+    item_type_map[item_type] = EXCEPTIONS[item_type]
+
+  # Future-proofing: Defensively check for duplicates in the item type map.
+  # This will catch any more nonsense like the Battle Sister changes.
+  for item_type_string in set(item_type_map.values()):
+    num_strings = len([_ for _ in item_type_map.values() if _==item_type_string])
+    if num_strings>1:
+      affected = [_ for _ in item_type_map if item_type_map[_]==item_type_string]
+      print('Warning: Duplicate item type strings found: "'+str(item_type_string)+'"')
+      print('         Affected items:')
+      for aff in affected:
+        print('           '+str(aff))
 
   return ench_str_map, item_type_map
 
